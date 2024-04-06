481d7ba23d94ecf4d737fa0c65ed411ddcfd5cca
dejito <deerealboy@gmail.com>
2024-04-06 18:22:20 +0100

    welcome screen implementation in progress

--- lib/pages/welcome/welcome_screen.dart
+++ lib/pages/welcome/welcome_screen.dart
@@ -23,7 +23,32 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             children: [
               PageView(
                 children: [
-                  SizedBox()
+                  SizedBox(
+                    width: 345.w,
+                    height: 345.w,
+                    child: Text(
+                      "Image One",
+                    ),
+                  ),
+                  Container(
+                    child: Text("See First Learning!",
+                      style: TextStyle(
+                        fontSize: 24.sp,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 375.w,
+                    padding: EdgeInsets.symmetric(horizontal: 30.w),
+                    child: Text(
+                      "Forget about a for apple in two learning",
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(.5),
+                        fontSize: 14.sp,
+
+                      ),
+                    ),
+                  )
                 ],
               ),
               Positioned(
