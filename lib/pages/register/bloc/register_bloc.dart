c250071ba7aac96dad3f4ec647b6ad56a0b17aff
dejito <deerealboy@gmail.com>
2024-04-13 20:45:39 +0100

    register bloc bloc added

--- lib/pages/register/bloc/register_bloc.dart
+++ lib/pages/register/bloc/register_bloc.dart
@@ -1,18 +1,30 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 
 part 'register_event.dart';
+
 part 'register_state.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
   RegisterBloc() : super(RegisterInitial()) {
     on<RegisterEvent>((event, emit) {});
 
-    on<UsernameEvent>((event, emit) {});
-
+    on<UsernameEvent>(_usernameEvent);
+    on<EmailEvent>(_emailEvent);
+    on<PasswordEvent>(_passwordEvent);
+    on<RePasswordEvent>(_rePasswordEvent);
   }
-  void _usernameEvent(EmailEvent event, Emitter<RegisterState> emit) {
-    emit(state.copyWith(email: event));
-}
 
+  void _usernameEvent(UsernameEvent event, Emitter<RegisterState> emit) {
+    emit(state.copyWith(email: event.username));
+  }
+
+  void _emailEvent(EmailEvent event, Emitter<RegisterState> emit) {
+    emit(state.copyWith(email: event.email));
+  }
+  void _passwordEvent(PasswordEvent event, Emitter<RegisterState> emit) {
+    emit(state.copyWith(email: event.password));
+  }
+  void _rePasswordEvent(RePasswordEvent event, Emitter<RegisterState> emit) {
+    emit(state.copyWith(email: event.rePassword));
+  }
 }
