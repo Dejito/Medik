66914138dd0f0709ea7d57bac999001343553d25
dejito <deerealboy@gmail.com>
2024-04-17 11:20:26 +0100

    get/set bool on storage service implemented

--- lib/common/service/storage_service.dart
+++ lib/common/service/storage_service.dart
@@ -1,11 +1,22 @@
+import 'package:medik/common/values/constant.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
 class StorageService {
   late final SharedPreferences _pref;
 
-  Future<StorageService> init() async{
+  Future<StorageService> init() async {
     _pref = await SharedPreferences.getInstance();
     return this;
   }
+
+  Future<void> setBool() async{
+    _pref.setBool(AppConstant.storageDeviceOpenFirstTime, true);
+  }
+
+  bool getBool() {
+    final bool = _pref.getBool(AppConstant.storageDeviceOpenFirstTime) ?? false;
+    return bool;
+  }
+
 }
